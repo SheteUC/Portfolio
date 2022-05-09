66f811be0a7b8aa618b3c3e0f858f9555d5adef3
Atharv <atharv@Shailajas-MacBook-Air.local>
2022-05-09 13:56:25 +0400

    completed the upload button

--- app.js
+++ app.js
@@ -34,3 +34,24 @@ function pageTransition() {
 }
 
 pageTransition();
+
+let downloadButton = document.querySelector('.btn-icon');
+if(downloadButton) {    
+  downloadButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    /* Start loading process. */
+    downloadButton.classList.add('loading');
+    
+    /* Set delay before switching from loading to success. */
+    window.setTimeout(function() {
+      downloadButton.classList.remove('loading');
+      downloadButton.classList.add('success');
+    }, 3000);
+    
+    /* Reset animation. */
+    window.setTimeout(function() {
+      downloadButton.classList.remove('success');
+    }, 8000);
+  });
+};
